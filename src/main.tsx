87958a929f3cc7860b0e59630035db32e0dcef30
EduardoBarros00 <lilkank4i20@gmail.com>
2026-09-03 12:15:54 -0300

    feat: load configurable background styles

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { AuthProvider } from './hooks/useAuth'
 import App from './App'
 import './styles.css'
+import './background.css'
 
 const queryClient = new QueryClient({
   defaultOptions: { queries: { staleTime: 30_000, retry: 1, refetchOnWindowFocus: false } },
