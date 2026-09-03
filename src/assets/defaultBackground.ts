ce70bf7341e64396f3be30e85b8e56a9824a4018
EduardoBarros00 <lilkank4i20@gmail.com>
2026-09-03 12:14:21 -0300

    feat: compose bundled LilKank background image

---
+++ src/assets/defaultBackground.ts
@@ -0,0 +1,8 @@
+import { backgroundChunk0 } from './background-chunk-0'
+import { backgroundChunk1 } from './background-chunk-1'
+import { backgroundChunk2a } from './background-chunk-2a'
+import { backgroundChunk2b } from './background-chunk-2b'
+import { backgroundChunk2c } from './background-chunk-2c'
+import { backgroundChunk2d } from './background-chunk-2d'
+
+export const DEFAULT_BACKGROUND = `data:image/webp;base64,${backgroundChunk0}${backgroundChunk1}${backgroundChunk2a}${backgroundChunk2b}${backgroundChunk2c}${backgroundChunk2d}`
